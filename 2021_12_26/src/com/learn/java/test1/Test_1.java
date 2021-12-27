b29b129d5d47229aa334afe99f66f78dff4e19de
xwj <1983318249@qq.com>
2021-12-27 23:42:02 +0800

    找重复

--- 2021_12_26/src/com/learn/java/test1/Test_1.java
+++ 2021_12_26/src/com/learn/java/test1/Test_1.java
@@ -1,5 +1,38 @@
 package com.learn.java.test1;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class Test_1 {
+    public static int func(int[] arr, int n){
+        int[] tmp = arr;
+        //先来排序
+        Arrays.sort(tmp);
+        //重复的肯定在一起
+        for (int i = 0; i < n-1; i++) {
+            if(tmp[i] == tmp[i+1]){
+                //找到了一个重复数字
+                //直接返回该数字
+                return tmp[i];
+            }
+        }
+        //没有返回一个-399
+        return -399;
+    }
 
+    public static void main(String[] args) {
+        //用来存放一组数
+        int[] arr = new int[100];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个整数：");
+        int n = sc.nextInt();
+        //输入数组中的数
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int ret = func(arr, n);
+        if(ret == -399){
+            System.out.println("没有重复的数！");
+        }else{
+            System.out.println("重复的数为："+ret);
+        }
+    }
 }
